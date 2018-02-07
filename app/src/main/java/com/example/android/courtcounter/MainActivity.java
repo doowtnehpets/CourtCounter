6bb9fd6398a2d572e34a9862a5dd278341773e10
doowtnehpets <doowtnehpets@gmail.com>
2018-02-06 20:00:36 -0700

    Implemented buttons for team B. Added reset button.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int team_A_score = 0;
+    int team_B_score = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +28,65 @@ public class MainActivity extends AppCompatActivity {
      * Increases Team A score by 3 points
      */
     public void team_A_3points(View view) {
-        displayForTeamA(3);
+        team_A_score += 3;
+        displayForTeamA(team_A_score);
     }
 
     /**
      * Increases Team A score by 2 points
      */
     public void team_A_2points(View view) {
-        displayForTeamA(2);
+        team_A_score += 2;
+        displayForTeamA(team_A_score);
     }
 
     /**
-     * Increases Team A score by 1 points
+     * Increases Team A score by 1 point
      */
     public void team_A_freethrow(View view) {
-        displayForTeamA(1);
+        team_A_score += 1;
+        displayForTeamA(team_A_score);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increases Team B score by 3 points
+     */
+    public void team_B_3points(View view) {
+        team_B_score += 3;
+        displayForTeamB(team_B_score);
+    }
+
+    /**
+     * Increases Team B score by 2 points
+     */
+    public void team_B_2points(View view) {
+        team_B_score += 2;
+        displayForTeamB(team_B_score);
+    }
+
+    /**
+     * Increases Team B score by 1 point
+     */
+    public void team_B_freethrow(View view) {
+        team_B_score += 1;
+        displayForTeamB(team_B_score);
+    }
+
+    /**
+     * Resets the score to zero for both teams
+     */
+    public void reset_score(View view) {
+        team_A_score = 0;
+        team_B_score = 0;
+        displayForTeamA(0);
+        displayForTeamB(0);
     }
 }
